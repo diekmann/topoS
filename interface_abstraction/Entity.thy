d739a3705818eb2f2e38ebc9741142b620a5ed43
Cornelius Diekmann <diekmann@net.in.tum.de>
2014-01-27 16:45:14 +0100

    link from

--- interface_abstraction/Entity.thy
+++ interface_abstraction/Entity.thy
@@ -22,39 +22,4 @@ section{*Basic Definitions*}
   value "\<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 2 \<rparr>"
 
 
-  text{*fwd is an entity's packet forward function: 
-      A packet arriving at port with a header (src, dst) is outputted to a set of ports*}
-  type_synonym 'v fwd_fun="port \<Rightarrow> ('v \<times> 'v) \<Rightarrow> port set"
-
-  
-  text{* A network consists of
-          A set of interfaces (ports at entities where packets are moved between)
-          A forwarding behaviour per entity
-          Links betweend interfaces (edges in a graph or cables in real world)*}
-  record 'v network = interfaces :: "'v interface set"
-                      forwarding :: "'v entity \<Rightarrow> 'v fwd_fun"
-                      links      :: "(('v interface) \<times> ('v interface)) set"
-
-
-  definition wellformed_network
-
-  value "\<lparr> interfaces = {\<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 1 \<rparr>,
-                         \<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 2 \<rparr>,
-                         \<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 3 \<rparr>,
-                         \<lparr> entity = Host ''Alice'', port = Port 1 \<rparr>,
-                         \<lparr> entity = Host ''Bob'', port = Port 1 \<rparr>,
-                         \<lparr> entity = Host ''Carl'', port = Port 1 \<rparr>},
-           forwarding = (\<lambda> e. \<lambda> p (srd,dst). {}),
-           links = {
-            (\<lparr> entity = Host ''Alice'', port = Port 1 \<rparr>, \<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 1 \<rparr>),
-            (\<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 1 \<rparr>, \<lparr> entity = Host ''Alice'', port = Port 1 \<rparr>),
-
-            (\<lparr> entity = Host ''Bob'', port = Port 1 \<rparr>, \<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 3 \<rparr>),
-            (\<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 3 \<rparr>, \<lparr> entity = Host ''Bob'', port = Port 1 \<rparr>),
-
-            (\<lparr> entity = Host ''Carl'', port = Port 1 \<rparr>, \<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 3 \<rparr>),
-            (\<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 3 \<rparr>, \<lparr> entity = Host ''Carl'', port = Port 1 \<rparr>)
-            }\<rparr>"
-
-
 end
