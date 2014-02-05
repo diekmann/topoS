f25e786e19518bade56888724da4e4af3a5d5569
Cornelius Diekmann <diekmann@net.in.tum.de>
2014-02-05 11:56:56 +0100

    refactored

--- interface_abstraction/Network.thy
+++ interface_abstraction/Network.thy
@@ -2,6 +2,8 @@ theory Network
 imports Entity
 begin
 
+(*examples and code equations are in Network_ex*)
+
 section{*A network consisting of entities*}
   text{*packet header*}
   type_synonym 'v hdr="('v entity \<times> 'v entity)" -- "packet header: (src address, dst address)"
@@ -62,44 +64,6 @@ section{*A network consisting of entities*}
         done
     end
 
-  text{*Example*}
-    (*
-      Alice   1
-              /\
-               |
-               |
-               \/
-                1                   2         -->  1 Bob
-                   ThreePortSwitch           /
-                           3  <-------------/
-                                             `---> 1 Carl
-    *)
-    definition "example_network = \<lparr> interfaces = {\<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 1 \<rparr>,
-                           \<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 2 \<rparr>,
-                           \<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 3 \<rparr>,
-                           \<lparr> entity = Host ''Alice'', port = Port 1 \<rparr>,
-                           \<lparr> entity = Host ''Bob'', port = Port 1 \<rparr>,
-                           \<lparr> entity = Host ''Carl'', port = Port 1 \<rparr>},
-             forwarding = (\<lambda> e. 
-                if e = NetworkBox ''threePortSwitch'' then 
-                  (\<lambda> p (src,dst). if p = Port 1 then Port ` {2,3} else if p = Port 2 then Port ` {1,3} else if p = Port 3 then Port ` {1,2} else {})
-                else
-                  (\<lambda> p (src,dst). if e = src then {Port 1} else {})), (*Hosts (not necessarily) send out their own packets and drop the rest*)
-             links = {
-              (\<lparr> entity = Host ''Alice'', port = Port 1 \<rparr>, \<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 1 \<rparr>),
-              (\<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 1 \<rparr>, \<lparr> entity = Host ''Alice'', port = Port 1 \<rparr>),
-  
-              (\<lparr> entity = Host ''Bob'', port = Port 1 \<rparr>, \<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 3 \<rparr>),
-              (\<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 3 \<rparr>, \<lparr> entity = Host ''Bob'', port = Port 1 \<rparr>),
-  
-              (\<lparr> entity = Host ''Carl'', port = Port 1 \<rparr>, \<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 3 \<rparr>),
-              (\<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 3 \<rparr>, \<lparr> entity = Host ''Carl'', port = Port 1 \<rparr>)
-              }\<rparr>"
-
-    interpretation example!: wellformed_network example_network
-      by(unfold_locales, auto simp add: example_network_def)
-    lemma wellformed_network_example_network: "wellformed_network example_network" by(unfold_locales)
-
 
     subsection{*A packet traverses a hop*}
 
@@ -110,18 +74,7 @@ section{*A network consisting of entities*}
       text{*succ moves packet along links. *}
       definition succ :: "'v network \<Rightarrow> 'v interface \<Rightarrow> ('v interface) set" where
         "succ N out_iface \<equiv> {in_iface. (out_iface, in_iface) \<in> links N}"
-        
-      definition succ_code :: "'v network \<Rightarrow> 'v interface \<Rightarrow> ('v interface) set" where
-        "succ_code N out_iface \<equiv> {in_iface \<in> interfaces N. (out_iface, in_iface) \<in> links N}"
-      lemma succ_code_correct: "wellformed_network N \<Longrightarrow> succ_code N = succ N"
-        apply(simp add: fun_eq_iff succ_def succ_code_def)
-        apply(drule wellformed_network.snd_links)
-        by force
-
-      text{*Example*}
-        lemma "succ_code example_network \<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 3 \<rparr> = 
-          {\<lparr>entity = Host ''Bob'', port = Port 1\<rparr>, \<lparr>entity = Host ''Carl'', port = Port 1\<rparr>}" by eval
-
+  
       text{*A packet in network N with header hdr traverses hop hop. The output is the set of input interface at the nex hops*}
       definition traverse :: "'v network \<Rightarrow> 'v hdr \<Rightarrow> 'v interface \<Rightarrow> ('v interface) set" where
         "traverse N hdr hop \<equiv> UNION (((forwarding N) (entity hop)) (port hop) hdr) (\<lambda>p. succ N \<lparr>entity = entity hop, port = p\<rparr>)"
@@ -132,22 +85,7 @@ section{*A network consisting of entities*}
         apply(drule wellformed_network.snd_links)
         by force
 
-      definition traverse_code :: "'v network \<Rightarrow> 'v hdr \<Rightarrow> 'v interface \<Rightarrow> ('v interface) set" where
-        "traverse_code N hdr hop \<equiv> UNION (((forwarding N) (entity hop)) (port hop) hdr) (\<lambda>p. succ_code N \<lparr>entity = entity hop, port = p\<rparr>)"
-      lemma traverse_code_correct: "wellformed_network N \<Longrightarrow> traverse_code N = traverse N"
-        apply(simp add: fun_eq_iff traverse_code_def traverse_def)
-        by(drule succ_code_correct, simp)
-
-
-      text{*Example*}
-        text{*Example: alice sends out packet, it arrives at switch*}
-        lemma example_network_ex1:"traverse_code example_network (Host ''Alice'', Host ''Bob'') \<lparr> entity = Host ''Alice'', port = Port 1 \<rparr>  = {\<lparr>entity = NetworkBox ''threePortSwitch'', port = Port 1\<rparr>}" by eval
-        text{*Example cont.: Switch forwards packet to Bob and Carl*}
-        lemma example_network_ex2: "traverse_code example_network (Host ''Alice'', Host ''Bob'')  \<lparr>entity = NetworkBox ''threePortSwitch'', port = Port 1\<rparr> = {\<lparr>entity = Host ''Carl'', port = Port 1\<rparr>, \<lparr>entity = Host ''Bob'', port = Port 1\<rparr>}" by eval
-        text{*Example cont.: Carl accepts packet (or drops it, he does not forward it)*}
-        lemma example_network_ex3: "traverse_code example_network (Host ''Alice'', Host ''Bob'') \<lparr>entity = Host ''Bob'', port = Port 1\<rparr> = {}" by eval
-        lemma example_network_ex4: "traverse_code example_network (Host ''Alice'', Host ''Bob'') \<lparr>entity = Host ''Carl'', port = Port 1\<rparr> = {}" by eval
-
+ 
 
     subsection {*Reachable interfaces*}
       text{*
@@ -171,101 +109,10 @@ section{*A network consisting of entities*}
         apply(simp)
         using traverse_subseteq_interfaces[OF wf_N] by fast
 
-      text{*Example*}
-        lemma "\<lparr> entity = Host ''Carl'', port = Port 1 \<rparr> \<in> reachable example_network (Host ''Alice'', Host ''Bob'') \<lparr>entity = Host ''Alice'', port = Port 1\<rparr>"
-          apply(rule reachable.intros(2))
-          apply(rule reachable.intros(2))
-          apply(rule reachable.intros(1))
-          (*apply(rule HOL.sym[of _ "entity \<lparr>entity = Host ''Alice'', port = Port (Suc 0)\<rparr>"]) (*need to select manually*)*)
-          apply(simp add: example_network_def)
-          apply(subst traverse_code_correct[symmetric, OF wellformed_network_example_network])
-          apply(simp, subst example_network_ex1[simplified])
-          apply(simp)
-          apply(subst traverse_code_correct[symmetric, OF wellformed_network_example_network])
-          apply(simp, subst example_network_ex2[simplified])
-          apply(simp)
-          done
-        lemma "\<lparr>entity = NetworkBox ''threePortSwitch'', port = Port 1\<rparr> \<in> reachable example_network (Host ''Alice'', Host ''Bob'') \<lparr>entity = Host ''Alice'', port = Port 1\<rparr>"
-          apply(rule reachable.intros(2))
-          apply(rule reachable.intros(1))
-          apply(simp add: example_network_def)
-          apply(subst traverse_code_correct[symmetric, OF wellformed_network_example_network])
-          apply(simp, subst example_network_ex1[simplified], simp)
-          done
-        lemma "x \<in> reachable example_network (Host ''Alice'', Host ''Bob'')  \<lparr>entity = Host ''Alice'', port = Port 1\<rparr> \<Longrightarrow> 
-          x \<in> {\<lparr> entity = Host ''Alice'', port = Port 1 \<rparr>,
-               \<lparr> entity = Host ''Carl'', port = Port 1 \<rparr>,
-               \<lparr> entity = Host ''Bob'', port = Port 1 \<rparr>,
-               \<lparr>entity = NetworkBox ''threePortSwitch'', port = Port 1\<rparr>}"
-          apply(induction rule: reachable.induct)
-          apply(simp)
-          (*step*)
-          apply(case_tac "hop = \<lparr>entity = NetworkBox ''threePortSwitch'', port = Port 1\<rparr>", simp)
-          apply(subst(asm) traverse_code_correct[symmetric, OF wellformed_network_example_network])
-          apply(subst(asm) example_network_ex2[simplified])
-          apply(fast)
-          apply(case_tac "hop = \<lparr>entity = Host ''Bob'', port = Port 1\<rparr>", simp)
-          apply(subst(asm) traverse_code_correct[symmetric, OF wellformed_network_example_network])
-          apply(subst(asm) example_network_ex3[simplified])
-          apply(fast)
-          apply(case_tac "hop = \<lparr>entity = Host ''Carl'', port = Port 1\<rparr>", simp)
-          apply(subst(asm) traverse_code_correct[symmetric, OF wellformed_network_example_network])
-          apply(subst(asm) example_network_ex4[simplified])
-          apply(fast)
-          apply(simp)
-          apply(subst(asm) traverse_code_correct[symmetric, OF wellformed_network_example_network])
-          apply(subst(asm) example_network_ex1[simplified])
-          by simp
-
     subsection{*The view of a packet*}
       definition view :: "'v network \<Rightarrow> 'v hdr \<Rightarrow> (('v interface) \<times> ('v interface)) set" where
         "view N hdr = {(src, dst). src \<in> interfaces N \<and> dst \<in> traverse N hdr src}"
 
-
-      definition view_code :: "'v network \<Rightarrow> 'v hdr \<Rightarrow> (('v interface) \<times> ('v interface)) set" where
-        "view_code N hdr = { src_dst \<in> interfaces N \<times> interfaces N. case src_dst of (src, dst) \<Rightarrow> dst \<in> traverse_code N hdr src}"
-      lemma view_code_correct: "wellformed_network N \<Longrightarrow> view_code N = view N"
-        apply(simp add: view_code_def view_def fun_eq_iff traverse_code_correct)
-        apply(clarify)
-        apply(rule equalityI)
-        apply blast
-        using traverse_subseteq_interfaces by fast
-
-      lemma traverse_code_subseteq_interfaces: "src \<in> interfaces N \<Longrightarrow> dst \<in> traverse_code N hdr src \<Longrightarrow> dst \<in> interfaces N"
-        by(simp add: traverse_code_def succ_code_def)
-      (*more efficient, I guess. Only iterate over the interfaces*)
-      lemma[code_unfold]: "view_code N hdr = UNION (interfaces N) (\<lambda> src. {src} \<times> traverse_code N hdr src)"
-        apply(simp add: view_code_def)
-        apply(rule)
-        apply blast
-        apply(rule)
-        apply(clarify)
-        apply(fact traverse_code_subseteq_interfaces)
-        done
-
-        
-    (*
-      the view jumps over routers. It essentially removes the traverse function. For example, if a packet arrives at switch port 1, it can continue to Bob and Carl directly. The fact that this happens via port 3 is hidden
-
-      Alice   1 <-------------------.
-        /\     |                    |
-         |     |                    |
-         |     |          ----------------------.
-         |     \/        |          |           \/
-         |      1 ------\<acute>          2 ----------> 1 Bob
-         |      |  ThreePortSwitch  |       
-         `------|--------- 3        |
-                |                    `-------->  
-                 `----------------------------> 1 Carl
-    *)
-        lemma "view_code example_network (Host ''Alice'', Host ''Bob'') = {
-          (\<lparr>entity = NetworkBox ''threePortSwitch'', port = Port 1\<rparr>, \<lparr>entity = Host ''Bob'', port = Port 1\<rparr>),
-          (\<lparr>entity = NetworkBox ''threePortSwitch'', port = Port 1\<rparr>, \<lparr>entity = Host ''Carl'', port = Port 1\<rparr>),
-          (\<lparr>entity = NetworkBox ''threePortSwitch'', port = Port 2\<rparr>, \<lparr>entity = Host ''Alice'', port = Port 1\<rparr>),
-          (\<lparr>entity = NetworkBox ''threePortSwitch'', port = Port 2\<rparr>, \<lparr>entity = Host ''Bob'', port = Port 1\<rparr>),
-          (\<lparr>entity = NetworkBox ''threePortSwitch'', port = Port 2\<rparr>, \<lparr>entity = Host ''Carl'', port = Port 1\<rparr>),
-          (\<lparr>entity = NetworkBox ''threePortSwitch'', port = Port 3\<rparr>, \<lparr>entity = Host ''Alice'', port = Port 1\<rparr>),
-          (\<lparr>entity = Host ''Alice'', port = Port 1\<rparr>, \<lparr>entity = NetworkBox ''threePortSwitch'', port = Port 1\<rparr>)}" by eval
 
     (*the view transforms the graph into a new graph without the traverse function! *)
 
@@ -295,7 +142,9 @@ section{*A network consisting of entities*}
       apply (metis (full_types) reachable.intros(2))
       done
 
-section{*TEST of UNIO*}
+
+
+section{*TEST TEST TES TEST of UNIO*}
   lemma "UNION {1::nat,2,3} (\<lambda>n. {n+1}) = {2,3,4}" by eval
   lemma "UNION {1::nat,2,3} (\<lambda>n. {n+1}) = set (map (\<lambda>n. n+1) [1,2,3])" by eval
 
@@ -316,12 +165,5 @@ section{*TEST of UNIO*}
     begin
       
     end
-
-
-  
-
-
-  hide_const "example_network"
-  hide_fact example_network_ex1 example_network_ex2 example_network_ex3 example_network_ex4 wellformed_network_example_network 
 
 end
---
+++ interface_abstraction/Network_ex.thy
@@ -0,0 +1,173 @@
+theory Network_ex
+imports Network
+begin
+
+text{*Example*}
+(*
+  Alice   1
+          /\
+           |
+           |
+           \/
+            1                   2         -->  1 Bob
+               ThreePortSwitch           /
+                       3  <-------------/
+                                         `---> 1 Carl
+*)
+definition "example_network = \<lparr> interfaces = {\<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 1 \<rparr>,
+                       \<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 2 \<rparr>,
+                       \<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 3 \<rparr>,
+                       \<lparr> entity = Host ''Alice'', port = Port 1 \<rparr>,
+                       \<lparr> entity = Host ''Bob'', port = Port 1 \<rparr>,
+                       \<lparr> entity = Host ''Carl'', port = Port 1 \<rparr>},
+         forwarding = (\<lambda> e. 
+            if e = NetworkBox ''threePortSwitch'' then 
+              (\<lambda> p (src,dst). if p = Port 1 then Port ` {2,3} else if p = Port 2 then Port ` {1,3} else if p = Port 3 then Port ` {1,2} else {})
+            else
+              (\<lambda> p (src,dst). if e = src then {Port 1} else {})), (*Hosts (not necessarily) send out their own packets and drop the rest*)
+         links = {
+          (\<lparr> entity = Host ''Alice'', port = Port 1 \<rparr>, \<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 1 \<rparr>),
+          (\<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 1 \<rparr>, \<lparr> entity = Host ''Alice'', port = Port 1 \<rparr>),
+
+          (\<lparr> entity = Host ''Bob'', port = Port 1 \<rparr>, \<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 3 \<rparr>),
+          (\<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 3 \<rparr>, \<lparr> entity = Host ''Bob'', port = Port 1 \<rparr>),
+
+          (\<lparr> entity = Host ''Carl'', port = Port 1 \<rparr>, \<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 3 \<rparr>),
+          (\<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 3 \<rparr>, \<lparr> entity = Host ''Carl'', port = Port 1 \<rparr>)
+          }\<rparr>"
+
+interpretation example!: wellformed_network example_network
+  by(unfold_locales, auto simp add: example_network_def)
+lemma wellformed_network_example_network: "wellformed_network example_network" by(unfold_locales)
+
+
+subsection{*succ*}
+  definition succ_code :: "'v network \<Rightarrow> 'v interface \<Rightarrow> ('v interface) set" where
+    "succ_code N out_iface \<equiv> {in_iface \<in> interfaces N. (out_iface, in_iface) \<in> links N}"
+  lemma succ_code_correct: "wellformed_network N \<Longrightarrow> succ_code N = succ N"
+    apply(simp add: fun_eq_iff succ_def succ_code_def)
+    apply(drule wellformed_network.snd_links)
+    by force
+
+  text{*Example*}
+    lemma "succ_code example_network \<lparr> entity = NetworkBox ''threePortSwitch'', port = Port 3 \<rparr> = 
+      {\<lparr>entity = Host ''Bob'', port = Port 1\<rparr>, \<lparr>entity = Host ''Carl'', port = Port 1\<rparr>}" by eval
+
+subsection{*traverse*}
+  definition traverse_code :: "'v network \<Rightarrow> 'v hdr \<Rightarrow> 'v interface \<Rightarrow> ('v interface) set" where
+    "traverse_code N hdr hop \<equiv> UNION (((forwarding N) (entity hop)) (port hop) hdr) (\<lambda>p. succ_code N \<lparr>entity = entity hop, port = p\<rparr>)"
+  lemma traverse_code_correct: "wellformed_network N \<Longrightarrow> traverse_code N = traverse N"
+    apply(simp add: fun_eq_iff traverse_code_def traverse_def)
+    by(drule succ_code_correct, simp)
+
+
+  text{*Example*}
+    text{*Example: alice sends out packet, it arrives at switch*}
+    lemma example_network_ex1:"traverse_code example_network (Host ''Alice'', Host ''Bob'') \<lparr> entity = Host ''Alice'', port = Port 1 \<rparr>  = {\<lparr>entity = NetworkBox ''threePortSwitch'', port = Port 1\<rparr>}" by eval
+    text{*Example cont.: Switch forwards packet to Bob and Carl*}
+    lemma example_network_ex2: "traverse_code example_network (Host ''Alice'', Host ''Bob'')  \<lparr>entity = NetworkBox ''threePortSwitch'', port = Port 1\<rparr> = {\<lparr>entity = Host ''Carl'', port = Port 1\<rparr>, \<lparr>entity = Host ''Bob'', port = Port 1\<rparr>}" by eval
+    text{*Example cont.: Carl accepts packet (or drops it, he does not forward it)*}
+    lemma example_network_ex3: "traverse_code example_network (Host ''Alice'', Host ''Bob'') \<lparr>entity = Host ''Bob'', port = Port 1\<rparr> = {}" by eval
+    lemma example_network_ex4: "traverse_code example_network (Host ''Alice'', Host ''Bob'') \<lparr>entity = Host ''Carl'', port = Port 1\<rparr> = {}" by eval
+
+subsection{*reachable*}
+  text{*Example*}
+    lemma "\<lparr> entity = Host ''Carl'', port = Port 1 \<rparr> \<in> reachable example_network (Host ''Alice'', Host ''Bob'') \<lparr>entity = Host ''Alice'', port = Port 1\<rparr>"
+      apply(rule reachable.intros(2))
+      apply(rule reachable.intros(2))
+      apply(rule reachable.intros(1))
+      (*apply(rule HOL.sym[of _ "entity \<lparr>entity = Host ''Alice'', port = Port (Suc 0)\<rparr>"]) (*need to select manually*)*)
+      apply(simp add: example_network_def)
+      apply(subst traverse_code_correct[symmetric, OF wellformed_network_example_network])
+      apply(simp, subst example_network_ex1[simplified])
+      apply(simp)
+      apply(subst traverse_code_correct[symmetric, OF wellformed_network_example_network])
+      apply(simp, subst example_network_ex2[simplified])
+      apply(simp)
+      done
+    lemma "\<lparr>entity = NetworkBox ''threePortSwitch'', port = Port 1\<rparr> \<in> reachable example_network (Host ''Alice'', Host ''Bob'') \<lparr>entity = Host ''Alice'', port = Port 1\<rparr>"
+      apply(rule reachable.intros(2))
+      apply(rule reachable.intros(1))
+      apply(simp add: example_network_def)
+      apply(subst traverse_code_correct[symmetric, OF wellformed_network_example_network])
+      apply(simp, subst example_network_ex1[simplified], simp)
+      done
+    lemma "x \<in> reachable example_network (Host ''Alice'', Host ''Bob'')  \<lparr>entity = Host ''Alice'', port = Port 1\<rparr> \<Longrightarrow> 
+      x \<in> {\<lparr> entity = Host ''Alice'', port = Port 1 \<rparr>,
+           \<lparr> entity = Host ''Carl'', port = Port 1 \<rparr>,
+           \<lparr> entity = Host ''Bob'', port = Port 1 \<rparr>,
+           \<lparr>entity = NetworkBox ''threePortSwitch'', port = Port 1\<rparr>}"
+      apply(induction rule: reachable.induct)
+      apply(simp)
+      (*step*)
+      apply(case_tac "hop = \<lparr>entity = NetworkBox ''threePortSwitch'', port = Port 1\<rparr>", simp)
+      apply(subst(asm) traverse_code_correct[symmetric, OF wellformed_network_example_network])
+      apply(subst(asm) example_network_ex2[simplified])
+      apply(fast)
+      apply(case_tac "hop = \<lparr>entity = Host ''Bob'', port = Port 1\<rparr>", simp)
+      apply(subst(asm) traverse_code_correct[symmetric, OF wellformed_network_example_network])
+      apply(subst(asm) example_network_ex3[simplified])
+      apply(fast)
+      apply(case_tac "hop = \<lparr>entity = Host ''Carl'', port = Port 1\<rparr>", simp)
+      apply(subst(asm) traverse_code_correct[symmetric, OF wellformed_network_example_network])
+      apply(subst(asm) example_network_ex4[simplified])
+      apply(fast)
+      apply(simp)
+      apply(subst(asm) traverse_code_correct[symmetric, OF wellformed_network_example_network])
+      apply(subst(asm) example_network_ex1[simplified])
+      by simp
+
+
+subsection{*view*}
+  definition view_code :: "'v network \<Rightarrow> 'v hdr \<Rightarrow> (('v interface) \<times> ('v interface)) set" where
+    "view_code N hdr = { src_dst \<in> interfaces N \<times> interfaces N. case src_dst of (src, dst) \<Rightarrow> dst \<in> traverse_code N hdr src}"
+  lemma view_code_correct: "wellformed_network N \<Longrightarrow> view_code N = view N"
+    apply(simp add: view_code_def view_def fun_eq_iff traverse_code_correct)
+    apply(clarify)
+    apply(rule equalityI)
+    apply blast
+    using traverse_subseteq_interfaces by fast
+
+  lemma traverse_code_subseteq_interfaces: "src \<in> interfaces N \<Longrightarrow> dst \<in> traverse_code N hdr src \<Longrightarrow> dst \<in> interfaces N"
+    by(simp add: traverse_code_def succ_code_def)
+  (*more efficient, I guess. Only iterate over the interfaces*)
+  lemma[code_unfold]: "view_code N hdr = UNION (interfaces N) (\<lambda> src. {src} \<times> traverse_code N hdr src)"
+    apply(simp add: view_code_def)
+    apply(rule)
+    apply blast
+    apply(rule)
+    apply(clarify)
+    apply(fact traverse_code_subseteq_interfaces)
+    done
+
+        
+  (*
+    the view jumps over routers. It essentially removes the traverse function. For example, if a packet arrives at switch port 1, it can continue to Bob and Carl directly. The fact that this happens via port 3 is hidden
+
+    Alice   1 <-------------------.
+      /\     |                    |
+       |     |                    |
+       |     |          ----------------------.
+       |     \/        |          |           \/
+       |      1 ------\<acute>          2 ----------> 1 Bob
+       |      |  ThreePortSwitch  |       
+       `------|--------- 3        |
+              |                    `-------->  
+               `----------------------------> 1 Carl
+  *)
+  lemma "view_code example_network (Host ''Alice'', Host ''Bob'') = {
+    (\<lparr>entity = NetworkBox ''threePortSwitch'', port = Port 1\<rparr>, \<lparr>entity = Host ''Bob'', port = Port 1\<rparr>),
+    (\<lparr>entity = NetworkBox ''threePortSwitch'', port = Port 1\<rparr>, \<lparr>entity = Host ''Carl'', port = Port 1\<rparr>),
+    (\<lparr>entity = NetworkBox ''threePortSwitch'', port = Port 2\<rparr>, \<lparr>entity = Host ''Alice'', port = Port 1\<rparr>),
+    (\<lparr>entity = NetworkBox ''threePortSwitch'', port = Port 2\<rparr>, \<lparr>entity = Host ''Bob'', port = Port 1\<rparr>),
+    (\<lparr>entity = NetworkBox ''threePortSwitch'', port = Port 2\<rparr>, \<lparr>entity = Host ''Carl'', port = Port 1\<rparr>),
+    (\<lparr>entity = NetworkBox ''threePortSwitch'', port = Port 3\<rparr>, \<lparr>entity = Host ''Alice'', port = Port 1\<rparr>),
+    (\<lparr>entity = Host ''Alice'', port = Port 1\<rparr>, \<lparr>entity = NetworkBox ''threePortSwitch'', port = Port 1\<rparr>)}" by eval
+
+    (*the view transforms the graph into a new graph without the traverse function! *)
+
+  
+  hide_const "example_network"
+  hide_fact example_network_ex1 example_network_ex2 example_network_ex3 example_network_ex4 wellformed_network_example_network 
+
+end
