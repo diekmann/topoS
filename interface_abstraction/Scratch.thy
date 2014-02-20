7915c92102e92ac159ce92d9e075b121cad399bd
Cornelius Diekmann <diekmann@net.in.tum.de>
2014-02-20 16:19:29 +0100

    testing ...

--- interface_abstraction/Scratch.thy
+++ interface_abstraction/Scratch.thy
@@ -1,6 +1,6 @@
 theory Scratch
 imports Main
-"../access_control_abstraction/thy/FiniteGraph"
+"../thy_lib/FiniteGraph"
 "../thy_lib/isabelle_afp/Graph_Theory/Pair_Digraph"
 begin
 
@@ -19,14 +19,55 @@ lemma "\<lbrakk> valid_graph G \<rbrakk> \<Longrightarrow> pair_fin_digraph (gra
 lemma "with_proj (graph_to_afp_graph G) =  \<lparr> verts = nodes G, arcs = edges G, tail = fst, head = snd \<rparr>"
   by(simp add: graph_to_afp_graph_def)
 
-definition "has_cycle" :: "'v graph \<Rightarrow> ('v \<times> 'v) awalk \<Rightarrow> bool" where
-  "has_cycle G p \<equiv> pre_digraph.cycle (with_proj (graph_to_afp_graph G)) p"
+definition "has_directed_cycle" :: "'v graph \<Rightarrow> ('v \<times> 'v) awalk \<Rightarrow> bool" where
+  "has_directed_cycle G p \<equiv> pre_digraph.cycle (with_proj (graph_to_afp_graph G)) p"
 
 lemma fixes G::"'v graph" and Gafp::"('v, 'v \<times> 'v) pre_digraph"
   assumes transl: "Gafp = with_proj (graph_to_afp_graph G)"
-  shows "has_cycle G p = (\<exists>u. pre_digraph.awalk Gafp u p u \<and> distinct (tl (pre_digraph.awalk_verts Gafp u p)) \<and> p \<noteq> [])"
-  by(simp add: has_cycle_def pre_digraph.cycle_def transl)
+  shows "has_directed_cycle G p = (\<exists>u. pre_digraph.awalk Gafp u p u \<and> distinct (tl (pre_digraph.awalk_verts Gafp u p)) \<and> p \<noteq> [])"
+  by(simp add: has_directed_cycle_def pre_digraph.cycle_def transl)
 
+
+definition acyclic :: "('v, 'v \<times> 'v) pre_digraph \<Rightarrow> bool" where
+  "acyclic G \<equiv> \<forall> u p v. pre_digraph.awalk G u p v \<longrightarrow> \<not> pre_digraph.cycle G p"
+
+lemma acyclic_alt: "wf_digraph G \<Longrightarrow> acyclic G = (\<forall> u \<in> verts G. \<forall> v \<in> verts G. \<forall> p. pre_digraph.awalk G u p v \<longrightarrow> \<not> pre_digraph.cycle G p)"
+  apply(simp add: acyclic_def wf_digraph_def)
+  by (metis (full_types) pre_digraph.awalk_def pre_digraph.cycle_def)
+
+lemma "\<not> acyclic \<lparr> pverts = {1::nat}, parcs = {(1,1)} \<rparr>"
+  apply(simp add: acyclic_def with_proj_def)
+  apply(rule_tac x="1" in exI)
+  apply(rule_tac x="[(1,1)]" in exI)
+  apply(rule conjI)
+  apply(rule_tac x="1" in exI)
+  apply(simp add: pre_digraph.awalk_def)
+  apply(simp add: pre_digraph.cas.simps)
+  apply(simp add: pre_digraph.cycle_def)
+  apply(simp add: pre_digraph.awalk_def)
+  apply(simp add: pre_digraph.cas.simps)
+  apply(simp add: pre_digraph.awalk_verts.simps)
+  done
+lemma "\<not> acyclic \<lparr> pverts = {1::nat,2,3}, parcs = {(1,2),(2,3),(3,1)} \<rparr>"
+  apply(simp add: acyclic_def with_proj_def)
+  apply(rule_tac x="1" in exI)
+  apply(rule_tac x="[(1,2),(2,3),(3,1)]" in exI)
+  apply(rule conjI)
+  apply(rule_tac x="1" in exI)
+  apply(simp add: pre_digraph.awalk_def)
+  apply(simp add: pre_digraph.cas.simps)
+  apply(simp add: pre_digraph.cycle_def)
+  apply(simp add: pre_digraph.awalk_def)
+  apply(simp add: pre_digraph.cas.simps)
+  apply(simp add: pre_digraph.awalk_verts.simps)
+  done
+lemma "acyclic \<lparr> pverts = {1::nat,2,3}, parcs = {(1,2),(2,3),(1,3)} \<rparr>"
+  apply(subgoal_tac "wf_digraph \<lparr> pverts = {1::nat,2,3}, parcs = {(1,2),(2,3),(1,3)} \<rparr>")
+  apply(unfold_locales, auto)
+  apply(simp add: acyclic_alt with_proj_def)
+  apply(thin_tac "wf_digraph ?x")
+  apply(auto)
+  oops (*need a methot to calculate awalks*)
 
 
 section{*TEST TEST TES TEST of UNIO*}
