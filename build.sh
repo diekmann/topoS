04e33e20100449a40f2c5748aae6898a16fe063e
Cornelius Diekmann <diekmann@net.in.tum.de>
2014-06-27 15:01:46 +0200

    fixed startup script
    do not use skip_proofs when building pdf

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd Network_Security_Policy_Verification
 export AFP=$(readlink -f "../isabelle_afp")
-isabelle build -d "./" -o document=pdf -o skip_proofs -vc Network_Security_Policy_Verification &&
+
+echo "quick_and_dirty"
+
+isabelle build -d "./" -o document=pdf -o quick_and_dirty -vc Network_Security_Policy_Verification &&
   isabelle build -d "./" -vc Network_Security_Policy_Verification_Examples
