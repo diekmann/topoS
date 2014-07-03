dd4f3bea400d1dd3e0b78e0c2f927a48651c9811
Cornelius Diekmann <diekmann@net.in.tum.de>
2014-07-03 15:20:18 +0200

    tuned build script and root.tex

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ export AFP=$(readlink -f "../isabelle_afp")
 #echo "quick_and_dirty"
 #-o quick_and_dirty
 
-isabelle build -d "./" -o document=pdf -v Network_Security_Policy_Verification &&
-  isabelle build -d "./" -v Network_Security_Policy_Verification_Examples
+isabelle build -d "./" -b -o "document=pdf" -o "document_variants=document:outline=/proof,/ML" -vc Network_Security_Policy_Verification &&
+  isabelle build -d "./" -b -v Network_Security_Policy_Verification_Examples
