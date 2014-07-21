a1452cafc57eccb5c91216895e75b957bdb7f7c1
Cornelius Diekmann <diekmann@net.in.tum.de>
2014-07-21 15:23:35 +0200

    tuned

--- Network_Security_Policy_Verification/Lib/ML_GraphViz_Config.thy
+++ Network_Security_Policy_Verification/Lib/ML_GraphViz_Config.thy
@@ -12,33 +12,18 @@ end
 
 structure Graphviz_Platform_Config: GRAPHVIZ_PLATFORM_CONFIG =
 struct
-
-  datatype platform = Linux | MacOs | Windows;
-  local
-    val platform_str = getenv "ML_PLATFORM";
-  in
-    val platform = if (String.isSuffix "linux" platform_str)
-      then
-        Linux
-      else if String.isSuffix "darwin" platform_str
-      then
-        MacOs
-      else if String.isSuffix "cygwin" platform_str
-      then
-        (warning "GRAPHVIZ_PLATFORM_CONFIG: never tested on windows"; Windows)
-      else
-        raise Fail "cannot determine operating system";
-  end;
-  
   (*Change your system config here*)
-  val (executable_dot: string, executable_pdf_viewer: string) = (case platform of 
-                   Linux => ("dot", getenv "PDF_VIEWER")
-                 | MacOs => ("dot", getenv "PDF_VIEWER")
-                 | Windows => ("dot", getenv "PDF_VIEWER"));
+  val (executable_dot: string, executable_pdf_viewer: string) = (
+            case getenv "ISABELLE_PLATFORM_FAMILY" of 
+                   "linux" => ("dot", getenv "PDF_VIEWER")
+                 | "macos" => ("dot", getenv "PDF_VIEWER")
+                 | "windows" => (warning "GRAPHVIZ_PLATFORM_CONFIG: never tested on windows"; ("dot", getenv "PDF_VIEWER"))
+                 | _ => raise Fail "cannot determine operating system"
+            );
   
   local
     fun check_executable e =
-      if Isabelle_System.bash ("which "^e) = 0 then (*TODO does `which` work on windows?*)
+      if Isabelle_System.bash ("which "^e) = 0 then
         () (* `which` already printed the path *)
       else 
        warning ("Command not available or not in $PATH: "^e);
