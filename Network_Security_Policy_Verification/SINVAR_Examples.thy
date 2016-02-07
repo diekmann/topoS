fa04e2cf5e37e147544c567286912001ca9048c2
Cornelius Diekmann <diekmann@net.in.tum.de>
2016-02-07 15:08:38 +0100

    Examples I will use to document stuff

---
+++ Network_Security_Policy_Verification/SINVAR_Examples.thy
@@ -0,0 +1,30 @@
+theory SINVAR_Examples
+imports
+  TopoS_Interface
+  TopoS_Interface_impl
+  TopoS_Library
+  TopoS_Composition_Theory
+  TopoS_Stateful_Policy
+  TopoS_Composition_Theory_impl
+  TopoS_Stateful_Policy_Algorithm
+  TopoS_Stateful_Policy_impl
+  TopoS_Impl
+begin
+
+definition make_max_policy :: "('a SecurityInvariant) list \<Rightarrow> 'a list \<Rightarrow> 'a list_graph" where
+  "make_max_policy sinvars V \<equiv> generate_valid_topology sinvars \<lparr>nodesL = V, edgesL = List.product V V \<rparr>"
+
+
+definition exampleG :: "nat list_graph" where
+  "exampleG \<equiv> \<lparr> nodesL = [1, 2, 3],
+                edgesL = [(1,2), (2,3)]\<rparr>"
+
+definition CommWith_m::"(nat SecurityInvariant)" where
+    "CommWith_m \<equiv> new_configured_list_SecurityInvariant SINVAR_LIB_ACLcommunicateWith \<lparr> 
+          node_properties = [
+                  1 \<mapsto> AccessList [2,3],
+                  2 \<mapsto> AccessList [3]], 
+          model_global_properties = () 
+          \<rparr>"
+
+value[code] "make_max_policy [CommWith_m] [1,2,3]"
