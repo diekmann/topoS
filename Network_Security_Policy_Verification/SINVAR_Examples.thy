5738ab3dc3480d9fbb3846c2c079178cb25e3230
Cornelius Diekmann <diekmann@net.in.tum.de>
2016-02-08 10:36:30 +0100

    examples

--- Network_Security_Policy_Verification/SINVAR_Examples.thy
+++ Network_Security_Policy_Verification/SINVAR_Examples.thy
@@ -30,10 +30,23 @@ definition CommWith_m::"(nat SecurityInvariant)" where
 
 text{*Experimental: the config (only one) can be added to the end.*}
 ML_val{*
-visualize_graph_header @{context} @{term "security_invariants"} @{term "policy"} @{term "BLP_security_clearances"};
+visualize_graph_header @{context} @{term "[CommWith_m]"} @{term "exampleG"} @{term "[
+                  (1::nat) \<mapsto> [2::nat,3],
+                  2 \<mapsto> [3]]"};
 *}
-
 
 value[code] "make_policy [CommWith_m] [1,2,3]"
 value[code] "implc_offending_flows CommWith_m \<lparr>nodesL = [1,2,3,4], edgesL = List.product [1,2,3,4] [1,2,3,4] \<rparr>"
 value[code] "make_policy [CommWith_m] [1,2,3,4]"
+
+
+ML_val{*
+visualize_graph @{context} @{term "[ new_configured_list_SecurityInvariant SINVAR_LIB_ACLcommunicateWith \<lparr> 
+          node_properties = [
+                  1::nat \<mapsto> [1,2,3],
+                  2 \<mapsto> [1,2,3,4], 
+                  3 \<mapsto> [1,2,3,4], 
+                  4 \<mapsto> [1,2,3,4]],
+          model_global_properties = () 
+          \<rparr>]"} @{term "\<lparr>nodesL = [1::nat,2,3,4], edgesL = [(1,2), (1,3), (2,3), (3, 4)] \<rparr>"};
+*}
