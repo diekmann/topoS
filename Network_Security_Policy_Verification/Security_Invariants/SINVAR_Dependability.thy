e0af59b9f0f95b653d0e1dc47cf9e99e7a4b7453
Cornelius Diekmann <diekmann@net.in.tum.de>
2016-02-16 18:09:26 +0100

    minor cleanup

--- Network_Security_Policy_Verification/Security_Invariants/SINVAR_Dependability.thy
+++ Network_Security_Policy_Verification/Security_Invariants/SINVAR_Dependability.thy
@@ -82,11 +82,6 @@ text{* It does not matter whether we iterate over all edges or all nodes. We cho
        apply(simp_all)
       done
 
-
-
-
-lemma unique_default_example_succ_tran: "succ_tran \<lparr>nodes = {vertex_1, vertex_2}, edges = {(vertex_1, vertex_2)}\<rparr> vertex_1 = {vertex_2}"
-using unique_default_example1 by blast
 (*
 lemma card_less_equal_trancl: "finite A \<Longrightarrow> card {e2. (aa, e2) \<in> (A - X)\<^sup>+} \<le> card {e2. (aa, e2) \<in> (A)\<^sup>+}"
 apply(subgoal_tac "{e2. (aa, e2) \<in> (A - X)\<^sup>+} \<subseteq> {e2. (aa, e2) \<in> (A)\<^sup>+}")
@@ -130,33 +125,45 @@ where default_node_properties = SINVAR_Dependability.default_node_properties
 and sinvar = SINVAR_Dependability.sinvar
 and verify_globals = verify_globals
   unfolding SINVAR_Dependability.default_node_properties_def
-  apply unfold_locales
-   apply simp
-   apply (simp add: SecurityInvariant_withOffendingFlows.set_offending_flows_def
-    SecurityInvariant_withOffendingFlows.is_offending_flows_min_set_def
-    SecurityInvariant_withOffendingFlows.is_offending_flows_def)
-   apply (simp split: prod.split_asm prod.split)
-   apply (simp add:graph_ops)
-   apply(clarify)
-   apply (metis gr0I le0)
-  apply(erule default_uniqueness_by_counterexample_ACS)
-  apply(simp)
-  apply (simp add: SecurityInvariant_withOffendingFlows.set_offending_flows_def
+  proof
+    fix G::"'a graph" and f nP
+    assume "wf_graph G" and "f \<in> set_offending_flows G nP"
+    thus "\<forall>i\<in>fst ` f. \<not> sinvar G (nP(i := 0))"
+     apply (simp add: SecurityInvariant_withOffendingFlows.set_offending_flows_def
       SecurityInvariant_withOffendingFlows.is_offending_flows_min_set_def
       SecurityInvariant_withOffendingFlows.is_offending_flows_def)
-  apply (simp add:graph_ops)
-  apply (simp split: prod.split_asm prod.split)
-  apply(rule_tac x="\<lparr> nodes={vertex_1,vertex_2}, edges = {(vertex_1,vertex_2)} \<rparr>" in exI, simp)
-  apply(rule conjI)
-   apply(simp add: wf_graph_def)
-  apply(rule_tac x="(\<lambda> x. 0)(vertex_1 := 0, vertex_2 := 0)" in exI, simp)
-  apply(rule conjI)
-   apply(simp add: unique_default_example_succ_tran num_reachable_def)
-  apply(rule_tac x="vertex_1" in exI, simp)
-  apply(rule_tac x="{(vertex_1,vertex_2)}" in exI, simp)
-  apply(simp add: unique_default_example_succ_tran num_reachable_def)
-  apply(simp add: succ_tran_def unique_default_example_simp1 unique_default_example_simp2)
-  done
+     apply (simp split: prod.split_asm prod.split)
+     apply (simp add:graph_ops)
+     apply(clarify)
+     apply (metis gr0I le0)
+     done
+  next
+   fix otherbot 
+   assume assm: "\<forall>G f nP i. wf_graph G \<and> f \<in> set_offending_flows G nP \<and> i \<in> fst ` f \<longrightarrow> \<not> sinvar G (nP(i := otherbot))"
+   have unique_default_example_succ_tran:
+    "succ_tran \<lparr>nodes = {vertex_1, vertex_2}, edges = {(vertex_1, vertex_2)}\<rparr> vertex_1 = {vertex_2}"
+    using unique_default_example1 by blast
+   from assm show "otherbot = 0"
+    apply -
+    apply(elim default_uniqueness_by_counterexample_ACS)
+    apply(simp)
+    apply (simp add: SecurityInvariant_withOffendingFlows.set_offending_flows_def
+        SecurityInvariant_withOffendingFlows.is_offending_flows_min_set_def
+        SecurityInvariant_withOffendingFlows.is_offending_flows_def)
+    apply (simp add:graph_ops)
+    apply (simp split: prod.split_asm prod.split)
+    apply(rule_tac x="\<lparr> nodes={vertex_1,vertex_2}, edges = {(vertex_1,vertex_2)} \<rparr>" in exI, simp)
+    apply(rule conjI)
+     apply(simp add: wf_graph_def)
+    apply(rule_tac x="(\<lambda> x. 0)(vertex_1 := 0, vertex_2 := 0)" in exI, simp)
+    apply(rule conjI)
+     apply(simp add: unique_default_example_succ_tran num_reachable_def)
+    apply(rule_tac x="vertex_1" in exI, simp)
+    apply(rule_tac x="{(vertex_1,vertex_2)}" in exI, simp)
+    apply(simp add: unique_default_example_succ_tran num_reachable_def)
+    apply(simp add: succ_tran_def unique_default_example_simp1 unique_default_example_simp2)
+    done
+  qed
 
   lemma TopoS_Dependability: "SecurityInvariant sinvar default_node_properties receiver_violation"
   unfolding receiver_violation_def by unfold_locales  
