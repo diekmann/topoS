31dbf47750cb5a35ccae4f8361e11e976ad934d0
Cornelius Diekmann <diekmann@net.in.tum.de>
2016-05-11 14:45:18 +0200

    SINVAR_TaintingTrusted with datatype invariant

--- Network_Security_Policy_Verification/Security_Invariants/SINVAR_TaintingTrusted.thy
+++ Network_Security_Policy_Verification/Security_Invariants/SINVAR_TaintingTrusted.thy
@@ -2,21 +2,56 @@ theory SINVAR_TaintingTrusted
 imports "../TopoS_Helper" (*SINVAR_BLPtrusted*)
 begin
 
-subsection {* SecurityInvariant Tainting for IFS  *}
+subsection {* SecurityInvariant Tainting with Untainting-Feature for IFS  *}
 
 context
 begin
-  qualified datatype taints = TaintsUntaints (taints: "string set") (untaints: "string set")
+  qualified datatype taints_raw = TaintsUntaints_Raw (taints_raw: "string set") (untaints_raw: "string set")
+
+  typedef taints = "{ts::taints_raw. untaints_raw ts \<subseteq> taints_raw ts}"
+    morphisms raw_of_taints Abs_taints
+  proof
+    show "TaintsUntaints_Raw {} {} \<in> {ts. untaints_raw ts \<subseteq> taints_raw ts}" by simp
+  qed
+  
+  setup_lifting type_definition_taints
+  
+  lemma taints_eq_iff:
+    "tsx = tsy \<longleftrightarrow> raw_of_taints tsx = raw_of_taints tsy"
+    by (simp add: raw_of_taints_inject)
+
+  definition taints :: "taints \<Rightarrow> string set" where
+    "taints ts \<equiv> taints_raw (raw_of_taints ts)"
+  definition untaints :: "taints \<Rightarrow> string set" where
+    "untaints ts \<equiv> untaints_raw (raw_of_taints ts)"
+
+  lemma taints_wellformedness: "untaints ts \<subseteq> taints ts"
+    using raw_of_taints taints_def untaints_def by auto
+
+
+  text \<open>Constructor for @{typ "taints"}:\<close>
+  
+  definition TaintsUntaints :: "string set \<Rightarrow> string set \<Rightarrow> taints" where
+    "TaintsUntaints ts uts = Abs_taints (TaintsUntaints_Raw (ts \<union> uts) uts)"
+
+  lemma raw_of_taints_TaintsUntaints:
+    "raw_of_taints (TaintsUntaints ts uts) = (TaintsUntaints_Raw (ts \<union> uts) uts)"
+    by (simp add: TaintsUntaints_def Abs_taints_inverse)
+
+  lemma taints_TaintsUntaints: "taints (TaintsUntaints ts uts) = ts \<union> uts"
+    by(simp add: taints_def raw_of_taints_TaintsUntaints)
+  lemma untaints_TaintsUntaints: "untaints (TaintsUntaints ts uts) = uts"
+    by(simp add: untaints_def raw_of_taints_TaintsUntaints)
 
   text{*The things in the first set are tainted, those in the second set are untainted.
     For example, a machine produces @{term "''foo''"}:
-      @{term "TaintsUntaints {''foo''} {}"}
+      @{term "TaintsUntaints_Raw {''foo''} {}"}
 
     For example, a machine consumes @{term "''foo''"} and @{term "''bar''"}, combines them in a 
     way that they are no longer critical and outputs @{term "''baz''"}:
-      @{term "TaintsUntaints {''foo'', ''bar'', ''baz''} {''foo'', ''bar''}"}
+      @{term "TaintsUntaints_Raw {''foo'', ''bar'', ''baz''} {''foo'', ''bar''}"}
+      abbreviated: @{term "TaintsUntaints {''baz''} {''foo'', ''bar''}"}
       *}
-    (*TODO: abbreviated: @{term "TaintsUntaints {''baz''} {''foo'', ''bar''}"}*)
 
   qualified definition default_node_properties :: "taints"
     where  "default_node_properties \<equiv> TaintsUntaints {} {}"
@@ -43,8 +78,7 @@ begin
       apply(rename_tac list_edges)
       apply(rule_tac ff="list_edges" in SecurityInvariant_withOffendingFlows.mono_imp_set_offending_flows_not_empty[OF sinvar_mono])
           apply(auto simp add: sinvar_def)
-       apply(auto simp add: sinvar_def SecurityInvariant_withOffendingFlows.is_offending_flows_def graph_ops)[1]
-      (*by (metis (no_types, lifting) "1"(2) DiffD1 DiffD2 SecurityInvariant_withOffendingFlows.is_offending_flows_def delete_edges_simp2 graph.select_convs(2) sinvar_def)+*)
+      apply(auto simp add: sinvar_def SecurityInvariant_withOffendingFlows.is_offending_flows_def graph_ops)
       done
     next
     case (2 N E E' nP) thus ?case by(simp add: sinvar_def) blast
@@ -55,11 +89,13 @@ begin
   
   
   text{*Needs the well-formedness condition that @{term "untaints otherbot \<subseteq> taints otherbot"}*}
-  private lemma Taints_def_unique: "untaints otherbot \<subseteq> taints otherbot \<Longrightarrow>
-      otherbot \<noteq> default_node_properties \<Longrightarrow>
+  private lemma Taints_def_unique: "otherbot \<noteq> default_node_properties \<Longrightarrow>
       \<exists>G p i f. wf_graph G \<and> \<not> sinvar G p \<and> f \<in> (SecurityInvariant_withOffendingFlows.set_offending_flows sinvar G p) \<and>
          sinvar (delete_edges G f) p \<and>
           i \<in> snd ` f \<and> sinvar G (p(i := otherbot)) "
+    apply(subgoal_tac "untaints otherbot \<subseteq> taints otherbot")
+    prefer 2
+     subgoal using taints_wellformedness by simp
     apply(simp add: default_node_properties_def)
     apply (simp add: SecurityInvariant_withOffendingFlows.set_offending_flows_def
         SecurityInvariant_withOffendingFlows.is_offending_flows_min_set_def
@@ -69,55 +105,40 @@ begin
     apply(rule_tac x="\<lparr> nodes=set [vertex_1,vertex_2], edges = set [(vertex_1,vertex_2)] \<rparr>" in exI, simp)
     apply(rule conjI)
      apply(simp add: wf_graph_def; fail)
-    apply(case_tac otherbot, rename_tac tnts untnts)
-    apply(subgoal_tac "\<exists>foo. foo \<in> tnts")
+    apply(subgoal_tac "\<exists>foo. foo \<in> taints otherbot")
      prefer 2
-     subgoal for tnts untnts (*TODO simplify*)
+     subgoal
+     apply(case_tac otherbot, rename_tac tsraw)
      apply(simp)
-     apply(subgoal_tac "tnts \<noteq> {}")
-      prefer 2 subgoal by blast
-     by blast
+     apply(subgoal_tac "taints_raw tsraw \<noteq> {}")
+      prefer 2 subgoal for tsraw
+      apply(case_tac tsraw)
+      apply(simp add: TaintsUntaints_def)
+      by fastforce
+     by (simp add: Abs_taints_inverse ex_in_conv taints_def)
     apply(elim exE, rename_tac foo)
     apply(rule_tac x="(\<lambda> x. default_node_properties)
-            (vertex_1 := TaintsUntaints tnts {}, vertex_2 := default_node_properties)" in exI)
+            (vertex_1 := TaintsUntaints {foo} {}, vertex_2 := default_node_properties)" in exI)
     apply(simp add: default_node_properties_def)
     apply(rule conjI)
-     apply(simp add: sinvar_def, blast)
+     apply(simp add: sinvar_def taints_TaintsUntaints untaints_TaintsUntaints; fail)
     apply(rule_tac x="vertex_2" in exI)
     apply(rule_tac x="set [(vertex_1,vertex_2)]" in exI, simp)
-    apply(simp add: sinvar_def, blast)
+    apply(simp add: sinvar_def taints_TaintsUntaints untaints_TaintsUntaints; fail)
     done
   
   
   
   subsubsection {*ENF*}
     private lemma Taints_ENF: "SecurityInvariant_withOffendingFlows.sinvar_all_edges_normal_form
-        sinvar (\<lambda>c1 c2.
-          case (c1, c2) of (TaintsUntaints tnts1 untnts1, TaintsUntaints tnts2 untnts2)
-          \<Rightarrow> tnts1 - untnts1 \<subseteq> tnts2)"
+        sinvar (\<lambda>c1 c2. taints c1 - untaints c1 \<subseteq> taints c2)"
       unfolding SecurityInvariant_withOffendingFlows.sinvar_all_edges_normal_form_def sinvar_def
-      apply(intro allI)
-      apply(rule iffI)
-       apply(intro ballI, rename_tac e)
-       apply(case_tac e, simp, rename_tac e1 e2)
-       apply(case_tac "nP e1", simp)
-       apply(case_tac "nP e2", simp)
-       apply(erule_tac x=e in ballE, simp_all)
-      apply(intro ballI, rename_tac e)
-      apply(case_tac e, simp, rename_tac e1 e2)
-      apply(case_tac "nP e1", simp)
-      apply(case_tac "nP e2", simp)
-      apply(erule_tac x=e in ballE, simp_all)
-      done
-    private lemma Taints_ENF_refl: "SecurityInvariant_withOffendingFlows.ENF_refl sinvar (\<lambda>c1 c2.
-          case (c1, c2) of (TaintsUntaints tnts1 untnts1, TaintsUntaints tnts2 untnts2)
-          \<Rightarrow> tnts1 - untnts1 \<subseteq> tnts2)"
+      by blast
+    private lemma Taints_ENF_refl: "SecurityInvariant_withOffendingFlows.ENF_refl
+        sinvar (\<lambda>c1 c2. taints c1 - untaints c1 \<subseteq> taints c2)"
       unfolding SecurityInvariant_withOffendingFlows.ENF_refl_def sinvar_def
       apply(intro conjI)
        subgoal using Taints_ENF by simp
-      apply(intro allI)
-      apply(rename_tac e)
-      apply(case_tac e, simp, rename_tac e1 e2)
       by auto
        
   
@@ -127,19 +148,11 @@ begin
        else 
         { {e \<in> edges G. case e of (e1,e2) \<Rightarrow> \<not> taints (nP e1) - untaints (nP e1) \<subseteq> taints (nP e2)} })"
     lemma Taints_offending_set: "SecurityInvariant_withOffendingFlows.set_offending_flows sinvar = Taints_offending_set"
-      apply(simp add: fun_eq_iff
+      by(auto simp add: fun_eq_iff
                         SecurityInvariant_withOffendingFlows.ENF_offending_set[OF Taints_ENF]
                         Taints_offending_set_def)
-      apply safe
-       apply(rename_tac G nP a b)
-       apply(case_tac "nP a", case_tac "nP b")
-       apply(simp)
-      apply(rename_tac G nP a b x)
-      apply(case_tac "nP a", case_tac "nP b")
-      apply(simp)
-      by blast 
      
-(*  
+ 
     interpretation Taints: SecurityInvariant_IFS sinvar default_node_properties
     rewrites "SecurityInvariant_withOffendingFlows.set_offending_flows sinvar = Taints_offending_set"
       unfolding receiver_violation_def
@@ -149,18 +162,14 @@ begin
         from 1(2) show ?case
         apply(intro ballI)
         apply(rule SecurityInvariant_withOffendingFlows.ENF_snds_refl_instance[OF Taints_ENF_refl])
-          apply(intro allI)
-          apply(rename_tac e1 e2)
-          apply(case_tac "nPa e1", case_tac "nPa e2")
-          apply(simp_all)
-        by blast
+          apply(simp add: sinvar_def taints_TaintsUntaints untaints_TaintsUntaints, blast)
+         by(simp)+
       next
       case 2 thus ?case
         apply(elim default_uniqueness_by_counterexample_IFS)
         apply(rule Taints_def_unique)
         apply(simp_all add: default_node_properties_def)
-        (*the well-formedness constraint remains*)
-        oops
+        done
       next
       case 3 show "set_offending_flows = Taints_offending_set" by(fact Taints_offending_set)
      qed
@@ -168,7 +177,7 @@ begin
   
     lemma TopoS_Tainting: "SecurityInvariant sinvar default_node_properties receiver_violation"
     unfolding receiver_violation_def by unfold_locales
-*)
+
 end
 
 end
