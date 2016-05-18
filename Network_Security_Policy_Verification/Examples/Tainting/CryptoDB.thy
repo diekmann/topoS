96c2d17013cecbbe80c977bdb7196848d1c08547
Marcel von Maltitz <vonmaltitz@net.in.tum.de>
2016-05-18 10:43:38 +0200

    Add locality constraint to CryptoDB

--- Network_Security_Policy_Verification/Examples/Tainting/CryptoDB.thy
+++ Network_Security_Policy_Verification/Examples/Tainting/CryptoDB.thy
@@ -16,28 +16,33 @@ definition policy :: "vString list_graph" where
   "policy \<equiv> \<lparr> nodesL = [
                         V ''A'', V ''A_encrypter'', V ''A_channel'',
                         V ''B'', V ''B_encrypter'', V ''B_channel'',
-                        V ''C'', V ''C_encrypter'', V ''C_channel'', V ''C_decrypter'',
+                        V ''C'', V ''C_encrypter'', V ''C_channel_in'', V ''C_channel_out'', V ''C_decrypter'',
                         V ''Adversary'',
                         V ''CryptoDB''],
               edgesL = [
               (V ''A'', V ''A_encrypter''), (V ''A_encrypter'', V ''A_channel''), (V ''A_channel'', V ''CryptoDB''),
               (V ''B'', V ''B_encrypter''), (V ''B_encrypter'', V ''B_channel''), (V ''B_channel'', V ''CryptoDB''),
-              (V ''C'', V ''C_encrypter''), (V ''C_encrypter'', V ''C_channel''), (V ''C_channel'', V ''CryptoDB''),
-              (V ''CryptoDB'', V ''C_channel''), (V ''C_channel'', V ''C_decrypter''), (V ''C_decrypter'', V ''C''),
+              (V ''C'', V ''C_encrypter''), (V ''C_encrypter'', V ''C_channel_out''), (V ''C_channel_out'', V ''CryptoDB''),
+              (V ''CryptoDB'', V ''C_channel_in''), (V ''C_channel_in'', V ''C_decrypter''), (V ''C_decrypter'', V ''C''),
               (V ''CryptoDB'', V ''Adversary''),
               (V ''Adversary'', V ''CryptoDB'')
               ] \<rparr>"
 
 context begin
   private definition "tainiting_host_attributes \<equiv> [
-                           V ''A'' \<mapsto> TaintsUntaints {''A''} {},
-                           V ''A_encrypter'' \<mapsto> TaintsUntaints {} {''A''},
-                           V ''B'' \<mapsto> TaintsUntaints {''B''} {},
-                           V ''B_encrypter'' \<mapsto> TaintsUntaints {} {''B''},
-                           V ''C'' \<mapsto> TaintsUntaints {''C''} {},
-                           V ''C_decrypter'' \<mapsto> TaintsUntaints {''C''} {},
-                           V ''C_encrypter'' \<mapsto> TaintsUntaints {} {''C''}
-                           (*V ''CryptoDB'' \<mapsto> TaintsUntaints {} {}*)
+                           V ''A'' \<mapsto> TaintsUntaints {''A'', ''A_local''} {},
+                           V ''A_encrypter'' \<mapsto> TaintsUntaints {''A_local''} {''A''},
+                           V ''A_channel'' \<mapsto> TaintsUntaints {''A_remote''} {''A_local'', ''gibberish''},
+                           V ''B'' \<mapsto> TaintsUntaints {''B'', ''B_local''} {},
+                           V ''B_encrypter'' \<mapsto> TaintsUntaints {''B_local''} {''B''},
+                           V ''B_channel'' \<mapsto> TaintsUntaints {''B_remote''} {''B_local'', ''gibberish''},
+                           V ''C'' \<mapsto> TaintsUntaints {''C'', ''C_local''} {},
+                           V ''C_encrypter'' \<mapsto> TaintsUntaints {''C_local''} {''C''},
+                           V ''C_decrypter'' \<mapsto> TaintsUntaints {''C'', ''C_local''} {},
+                           V ''C_channel_out'' \<mapsto> TaintsUntaints {''C_remote''} {''C_local''},
+                           V ''C_channel_in'' \<mapsto> TaintsUntaints {''C_local''} {''C_remote'', ''gibberish''},
+                           V ''CryptoDB'' \<mapsto> TaintsUntaints {''gibberish''} {''A_remote'', ''B_remote'', ''C_remote''},
+                           V ''Adversary'' \<mapsto> TaintsUntaints {''gibberish''} {}
                            ]"
   private lemma "dom (tainiting_host_attributes) \<subseteq> set (nodesL policy)" by(simp add: tainiting_host_attributes_def policy_def)
   definition "Tainting_m \<equiv> new_configured_list_SecurityInvariant SINVAR_LIB_TaintingTrusted \<lparr>
